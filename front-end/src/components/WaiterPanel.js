f46c8e7beda0796235bf7701cd26f518af507001
Mohamed Amin ED-DARYANI <doryaniamin66@gmail.com>
2025-08-19 22:56:34 +0100

    style

--- front-end/src/components/WaiterPanel.js
+++ front-end/src/components/WaiterPanel.js
@@ -76,8 +76,8 @@ export default function WaiterPanel() {
     <>
     
 
-    <div class="px-10 py-10 flex justify-between">
-    <div className="" style={{ width: "20%", backgroundColor : "#f4f6f8" }} class="flex justify-center  ">
+    <div class="px-10 py-10 flex justify-between ">
+    <div className="" style={{ width: "20%", backgroundColor : "#f4f6f8" }} class="flex justify-center  hidden xl:flex">
       <div class="bg-white w-80 h-max pb-20 pt-5 pl-5">
           <h1 class="font-bold mb-10">Menu</h1>
           {categories.map((cat) => (
@@ -91,23 +91,22 @@ export default function WaiterPanel() {
         
       ))}
       </div>
-    </div>
-    <div className="" style={{ width: "50%" }}>
-      
-      <div class="p-5 flex flex-wrap ">
       
     </div>
+    <div className="xl:w-5/10  full " >
+      
+      
       
     
-       <div className="product-grid" class=" flex flex-wrap justify-around">
+       <div className="product-grid" class=" flex flex-wrap justify-around items-around w-full mb-20 ">
        {menu.filter((item) => item.category_id === variable).map((item) => (
-                  <div className="product-card " key={item.id} class=" mt-10 mr-6 w-60 flex flex-col items-center justify-center shadow-lg p-10 rounded-lg bg-white">
+                  <div className="product-card " key={item.id} class=" mt-5 mr-2 w-40 md:w-60 flex flex-col items-center justify-center  shadow-lg p-10 rounded-lg bg-white">
                   <img src={item.url}></img>
                   <span>{item.name}</span>
                   <span>${item.price.toFixed(2)}</span>
                   <div class="w-full flex justify-between mt-10 items-center ">
                     <span>${item.price.toFixed(2)}</span>
-                    <span class="p-4  cursor-pointer text-2xl font-extrabold bg-[#f4f6f8] hover:text-white hover:bg-red-600 rounded-md" onClick={() => addToCart(item)}>+</span>
+                    <span class="p-2  cursor-pointer text-2xl font-extrabold bg-[#f4f6f8] hover:text-white hover:bg-red-600 rounded-md ml-2" onClick={() => addToCart(item)}>+</span>
                   </div>
                   {/* <button onClick={() => addToCart(item)} style={{  backgroundColor: "#4785f2"}} class="mt-5">Add</button> */}
                 </div>
@@ -115,7 +114,7 @@ export default function WaiterPanel() {
         </div>
       
     </div>
-    <div style={{ width: "20%" }} class="">
+    <div style={{ width: "20%" }} class="hidden xl:block">
       <h3>Cart</h3>
       <ul>
         <input
@@ -142,9 +141,43 @@ export default function WaiterPanel() {
         ))}
       </ul>
       <h4 class="mb-5">Total: ${totalPrice.toFixed(2)}</h4>
+      <button onClick={sendOrder} style={{  backgroundColor: "#4785f2"}} >Send Order</button>
+      <button onClick={clearcart} class="ml-5 gb-red-500 mt-3" style={{  backgroundColor: "red"}}>Clear order</button>
+    </div>
+    </div>
+    <div class="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 flex justify-between xl:hidden" id="divbuttom1" onClick= {() =>{document.getElementById("divbuttom1").classList.add("hidden");document.getElementById("divbuttom2").classList.remove("hidden")}}>
+        <div><h3>Number of items : {cart.length} Total to Pay: {totalPrice.toFixed(2)}$ <spam class="px-4 py-2 bg-blue-500 rounded-lg cursor-pointer ml-5 ">Send</spam></h3></div> <h3><spam class="text-lg text-white font-extrabold cursor-pointer">X</spam></h3>
+    </div>
+    <div class=" hidden fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4  xl:hidden" id="divbuttom2">
+        <spam class="w-full flex justify-between"><h3>Cart</h3> <h3 class="text-lg text-white font-extrabold cursor-pointer" onClick= {() =>{document.getElementById("divbuttom2").classList.add("hidden");document.getElementById("divbuttom1").classList.remove("hidden")}}>X</h3></spam>
+      <ul>
+        <input
+        placeholder="Table ID"
+        value={tableId}
+        onChange={(e) => setTableId(e.target.value)}
+      />
+        {cart.map((item) => (
+         
+          <li className="cart-item " key={item.id} class="bg-white text-black">
+             <img src="https://static.vecteezy.com/system/resources/thumbnails/023/742/327/small_2x/latte-coffee-isolated-illustration-ai-generative-free-png.png" class="w-15"></img>
+            {item.name} (${item.price}) x
+            <input
+              type="number"
+              min="1"
+              value={item.quantity}
+              onChange={(e) =>
+                updateQuantity(item.id, parseInt(e.target.value))
+              }
+              style={{ width: "50px", margin: "0 5px", color:"black" }}
+            />
+             ${(item.price * item.quantity).toFixed(2)}
+          </li>
+        ))}
+      </ul>
+      <h4 class="mb-5 ">Total: ${totalPrice.toFixed(2)}</h4>
       <button onClick={sendOrder} style={{  backgroundColor: "#4785f2"}}>Send Order</button>
       <button onClick={clearcart} class="ml-5 gb-red-500" style={{  backgroundColor: "red"}}>Clear order</button>
-    </div>
+    
     </div>
     </>
   );
