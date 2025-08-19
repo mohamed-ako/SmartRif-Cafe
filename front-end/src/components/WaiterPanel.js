6e5329393d79c68979ac6a0bcef6d39ecff2112d
Mohamed Amin ED-DARYANI <doryaniamin66@gmail.com>
2025-08-19 15:29:42 +0100

    added style to the admin page

--- front-end/src/components/WaiterPanel.js
+++ front-end/src/components/WaiterPanel.js
@@ -63,43 +63,59 @@ export default function WaiterPanel() {
     0
   );
 
-  //filter Cat 
+  //clear Cat 
+  function clearcart() {
+    setCart([])
+  }
+
+
+
+
   
   return (
     <>
     
 
     <div class="px-10 py-10 flex justify-between">
-
-    <div className="" style={{ width: "60%" }}>
-      <h2>Waiter Panel</h2>
-      <div class="p-5 flex flex-wrap ">
-      {categories.map((cat) => (
+    <div className="" style={{ width: "20%", backgroundColor : "#f4f6f8" }} class="flex justify-center  ">
+      <div class="bg-white w-80 h-max pb-20 pt-5 pl-5">
+          <h1 class="font-bold mb-10">Menu</h1>
+          {categories.map((cat) => (
         
         
-        <div key={cat.id} class="p-5 shadow-lg rounded-lg w-30 ml-5 flex flex-col justify-center justify-items-center bg-white" onClick={()=>setVariable(cat.id)}>
-                  <img src={cat.url}></img>
-                  <h3>{cat.name}</h3>
+        <div key={cat.id} class="flex items-center hover:bg-red-600  group cursor-pointer border-b-1 border-gray-100" onClick={()=>setVariable(cat.id)}>
+                  <img src="https://static.vecteezy.com/system/resources/thumbnails/023/742/327/small_2x/latte-coffee-isolated-illustration-ai-generative-free-png.png" class="w-10 mr-5"></img>
+                  <h3 class="group-hover:text-white e">{cat.name}</h3>
           
         </div>
         
       ))}
+      </div>
+    </div>
+    <div className="" style={{ width: "50%" }}>
+      
+      <div class="p-5 flex flex-wrap ">
+      
     </div>
       
     
        <div className="product-grid" class=" flex flex-wrap justify-around">
        {menu.filter((item) => item.category_id === variable).map((item) => (
-                  <div className="product-card " key={item.id} class=" mt-10 mr-10 w-50 flex flex-col items-center justify-center shadow-lg p-10 rounded-lg bg-white">
+                  <div className="product-card " key={item.id} class=" mt-10 mr-6 w-60 flex flex-col items-center justify-center shadow-lg p-10 rounded-lg bg-white">
                   <img src={item.url}></img>
                   <span>{item.name}</span>
                   <span>${item.price.toFixed(2)}</span>
-                  <button onClick={() => addToCart(item)}>Add</button>
+                  <div class="w-full flex justify-between mt-10 items-center ">
+                    <span>${item.price.toFixed(2)}</span>
+                    <span class="p-4  cursor-pointer text-2xl font-extrabold bg-[#f4f6f8] hover:text-white hover:bg-red-600 rounded-md" onClick={() => addToCart(item)}>+</span>
+                  </div>
+                  {/* <button onClick={() => addToCart(item)} style={{  backgroundColor: "#4785f2"}} class="mt-5">Add</button> */}
                 </div>
        ))}
         </div>
       
     </div>
-    <div style={{ width: "30%" }} class="">
+    <div style={{ width: "20%" }} class="">
       <h3>Cart</h3>
       <ul>
         <input
@@ -121,12 +137,13 @@ export default function WaiterPanel() {
               }
               style={{ width: "50px", margin: "0 5px" }}
             />
-            = ${(item.price * item.quantity).toFixed(2)}
+             ${(item.price * item.quantity).toFixed(2)}
           </li>
         ))}
       </ul>
-      <h4>Total: ${totalPrice.toFixed(2)}</h4>
-      <button onClick={sendOrder}>Send Order</button>
+      <h4 class="mb-5">Total: ${totalPrice.toFixed(2)}</h4>
+      <button onClick={sendOrder} style={{  backgroundColor: "#4785f2"}}>Send Order</button>
+      <button onClick={clearcart} class="ml-5 gb-red-500" style={{  backgroundColor: "red"}}>Clear order</button>
     </div>
     </div>
     </>
